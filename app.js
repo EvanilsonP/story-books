6906871605f93bac245ececf858c51197699c605
EvanilsonP <pevanilson99@gmail.com>
2023-05-12 13:26:11 -0300

    Initial express setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const dotenv = require('dotenv');
+// load config
+dotenv.config({ path: './config/config.env'});
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
